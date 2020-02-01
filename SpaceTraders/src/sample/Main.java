2a169f223889d51c914c30b8eb3aac9f91379655
Shah, Shivam P <sshah459@gatech.edu>
2020-02-01 16:49:38 -0500

    Ran Checkstyle using 8.24
    Fixed errors found by Checkstyle

--- SpaceTraders/src/sample/Main.java
+++ SpaceTraders/src/sample/Main.java
@@ -15,7 +15,7 @@ import javafx.stage.Stage;
 public class Main extends Application {
 
     @Override
-    public void start(Stage primaryStage) throws Exception{
+    public void start(Stage primaryStage) throws Exception {
         Parent root = FXMLLoader.load(getClass().getResource("sample.fxml"));
         //Initializes height and width variables
         int height = 480;
@@ -31,7 +31,7 @@ public class Main extends Application {
 
         //Initializes the Scene for Configuration Screen
         BorderPane configurationPane = new BorderPane();
-        Scene configurationScreen = new Scene(configurationPane, width,height);
+        Scene configurationScreen = new Scene(configurationPane, width, height);
         Button configurationScreenDone = new Button("Done");
         configurationPane.setBottom(configurationScreenDone);
         Person person = new Person();
@@ -41,30 +41,28 @@ public class Main extends Application {
         Label difficultyLabel = new Label("Difficulty: " + person.getDifficulty());
         Label creditsLabel = new Label("Credits: " + String.valueOf(person.getCredits()));
         VBox charInfo = new VBox(8);
-        charInfo.getChildren().addAll(nameLabel,difficultyLabel,creditsLabel);
+        charInfo.getChildren().addAll(nameLabel, difficultyLabel, creditsLabel);
         Label engineeringLabel = new Label("Engineering: " + person.getEngineeringPoints());
         Label fighterLabel = new Label("Fighter: " + person.getFighterPoints());
         Label pilotLabel = new Label("Pilot: " + person.getPilotPoints());
         Label merchantLabel = new Label("Merchant: " + person.getMerchantPoints());
         VBox skillInfo = new VBox(8);
-        skillInfo.getChildren().addAll(engineeringLabel,fighterLabel,pilotLabel,merchantLabel);
+        skillInfo.getChildren().addAll(engineeringLabel, fighterLabel, pilotLabel, merchantLabel);
         characterSheetPane.getChildren().addAll(charInfo, skillInfo);
         Scene characterSheetScreen = new Scene(characterSheetPane, width, height);
         //Event for Welcome Screen Button and Configuration Screen Button
         startButton.setOnAction(new EventHandler() {
-
             @Override
             public void handle(Event event) {
                 primaryStage.setScene(configurationScreen);
             }
         });
         configurationScreenDone.setOnAction(new EventHandler() {
-
             @Override
             public void handle(Event event) {
                 primaryStage.setScene(characterSheetScreen);
             }
-       });
+        });
     }
 
 
