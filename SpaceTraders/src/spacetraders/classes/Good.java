95e90c2522f0bed05d7cf46e46f70f0a3ef726ea
Shah, Shivam P <sshah459@gatech.edu>
2020-02-24 14:03:58 -0500

    Updated classes with checkstyle fixes

--- SpaceTraders/src/spacetraders/classes/Good.java
+++ SpaceTraders/src/spacetraders/classes/Good.java
@@ -1,25 +1,26 @@
 package spacetraders.classes;
 
 public enum Good {
-    WOOD ("Wood", 50, 50, "fuelCapacity"),
-    OIL ("Oil", 100, 50, "fuelCapacity"),
-    PLUTONIUM ("Plutonium", 300, 100,"fuelCapacity"),
-    GODFUEL ("God Fuel", 500, 200, "fuelCapacity"),
+    WOOD("Wood", 50, 50, "fuelCapacity"),
+    OIL("Oil", 100, 50, "fuelCapacity"),
+    PLUTONIUM("Plutonium", 300, 100, "fuelCapacity"),
+    GODFUEL("God Fuel", 500, 200, "fuelCapacity"),
 
-    SMALLHULL ("Small Hull", 50, 2, "cargoCapacity"),
-    BIGHULL ("Big Hull", 200, 3, "cargoCapacity"),
-    ATTACHABLE ("Attachable Storage Container", 400, 5, "cargoCapacity"),
-    TARDIS ("Tardis Extension", 600, 10, "cargoCapacity"),
+    SMALLHULL("Small Hull", 50, 2, "cargoCapacity"),
+    BIGHULL("Big Hull", 200, 3, "cargoCapacity"),
+    ATTACHABLE("Attachable Storage Container", 400, 5, "cargoCapacity"),
+    TARDIS("Tardis Extension", 600, 10, "cargoCapacity"),
 
-    LIGHTFORCEFIELD ("Light Force Field", 200, 2,"health"),
-    MEDIUMFORCEFIELD ("Medium Force Field", 400, 4, "health"),
-    HEAVYFORCEFIELD ("Heavy Force Field", 600, 6, "health"),
-    DYSONSPHERE ("Dyson Sphere", 800, 8, "health"),
+    LIGHTFORCEFIELD("Light Force Field", 200, 2, "health"),
+    MEDIUMFORCEFIELD("Medium Force Field", 400, 4, "health"),
+    HEAVYFORCEFIELD("Heavy Force Field", 600, 6, "health"),
+    DYSONSPHERE("Dyson Sphere", 800, 8, "health"),
 
-    LASERS ("Lasers", 100, 2, "fighterPoints"),
-    MISSILES ("Missiles", 200, 4, "fighterPoints"),
-    LASERMISSILES ("Laser Missiles", 400, 6, "fighterPoints"),
-    SPACENUKES ("Space Nukes", 1000, 10, "fighterPoints");
+    LASERS("Lasers", 100, 2, "fighterPoints"),
+    MISSILES("Missiles", 200, 4, "fighterPoints"),
+    LASERMISSILES("Laser Missiles", 400, 6, "fighterPoints"),
+    SPACENUKES("Space Nukes", 1000, 10, "fighterPoints");
+
     private int basePrice;
     private String name;
     private int modFactor;
