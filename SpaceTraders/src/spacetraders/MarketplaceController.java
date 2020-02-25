3274abb07c75fbee08ef195112e3c8487656a92e
AdamPendry <55507747+AdamPendry@users.noreply.github.com>
2020-02-24 20:20:08 -0500

    Made some UI improvements

--- SpaceTraders/src/spacetraders/MarketplaceController.java
+++ SpaceTraders/src/spacetraders/MarketplaceController.java
@@ -1,12 +1,25 @@
 package spacetraders;
 
+import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
+import javafx.scene.control.Button;
+import javafx.scene.control.Label;
 
 import java.net.URL;
 import java.util.ResourceBundle;
 
 public class MarketplaceController implements Initializable {
 
+    private @FXML Button buy;
+    private @FXML Button sell;
+    private @FXML Button fuelGoodConfirm;
+    private @FXML Button cargoGoodConfirm;
+    private @FXML Button healthGoodConfirm;
+    private @FXML Button fighterGoodConfirm;
+    private @FXML Label fuelGood;
+    private @FXML Label cargoGood;
+    private @FXML Label healthGood;
+    private @FXML Label fighterGood;
 
     public static void basePrice() {
 
@@ -22,6 +35,9 @@ public class MarketplaceController implements Initializable {
 
     @Override
     public void initialize(URL location, ResourceBundle resources) {
+        buy.setText("Buy");
+        sell.setText("Sell");
+
 
     }
 }
